5d9d8a99ea1f2fd496ec700bb078c6e412090b58
Nikko <nikko.thorne@gmail.com>
2024-08-11 13:44:37 -0700

    Update

--- src/components/MainHeroImage.tsx
+++ src/components/MainHeroImage.tsx
@@ -1,18 +1,21 @@
 import React from 'react';
-
 import config from '../config/index.json';
 
 const MainHeroImage = () => {
-  const { mainHero } = config;
-  return (
-    <div className="lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2">
-      <img
-        className="h-56 w-full object-cover sm:h-72 md:h-96 lg:w-full lg:h-full"
-        src={mainHero.img}
-        alt="happy team image"
-      />
-    </div>
-  );
+    const { mainHero } = config;
+
+    return (
+        <div className="lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2">
+            {mainHero && mainHero.img && (
+                <img
+                    className="h-56 w-full object-cover sm:h-72 md:h-96 lg:w-full lg:h-full object-contain" // Change object-cover to object-contain
+                    src={mainHero.img}
+                    alt="smart home"
+                />
+
+            )}
+        </div>
+    );
 };
 
 export default MainHeroImage;
